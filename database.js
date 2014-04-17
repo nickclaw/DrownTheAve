90d72493b50bef57823e254f57a1435fb74e911f
Nicholas Clawson <nickclaw@gmail.com>
2014-04-17 01:02:42 -0700

    Make database.js actually work

--- database.js
+++ database.js
@@ -8,43 +8,48 @@ var toCrud = {
 };
 
 // add get, add, delete functions for each model
-for(key in toCrud) {
+for(var key in toCrud) {
 
-    /**
-     * Retrieves or searches for the record
-     * @param {object|Number} options|id
-     * @param {Function?} callback
-     * @return {Promise}
-     */
-    exports['get' + key] = function(options, callback) {
-        if (typeof options === 'number') {
-            options = {id: options};
+    // wrap it in the function so that key doesn't end up being overwritten
+    (function(exports, key) {
+
+        /**
+         * Retrieves or searches for the record
+         * @param {object|Number} options|id
+         * @param {Function?} callback
+         * @return {Promise}
+         */
+        exports['get' + key] = function(options, callback) {
+            if (typeof options === 'string') {
+                options = {_id: options};
+            }
+
+            return toCrud[key].find(options).exec(callback);
+        };
+
+        /**
+         * Creates a record
+         * @param {object} options
+         * @param {Function?} callback
+         * @return {Promise}
+         */
+        exports['add' + key] = function(options, callback) {
+            return toCrud[key].create(options, callback);
         }
 
-        return toCrud[key].find(options).exec(callback);
-    };
+        /**
+         * Deletes a record
+         * @param {object|Number} options|id
+         * @param {Function?} callback
+         * @return {Promise}
+         */
+        exports['delete' + key] = function(options, callback) {
+            if (typeof options === 'string') {
+                options = {_id: options};
+            }
 
-    /**
-     * Creates a record
-     * @param {object} options
-     * @param {Function?} callback
-     * @return {Promise}
-     */
-    exports['add' + key] = function(options, callback) {
-        return toCrud[key].create(options, callback);
-    }
-
-    /**
-     * Deletes a record
-     * @param {object|Number} options|id
-     * @param {Function?} callback
-     * @return {Promise}
-     */
-    exports['delete' + key] = function(options, callback) {
-        if (typeof options === 'number') {
-            options = {id: options};
+            return toCrud[key].remove(options).exec(callback);
         }
 
-        return toCrud[key].remove(options).exec(callback);
-    }
+    })(exports, key);
 }
--- routers/pageRouter.js
+++ routers/pageRouter.js
@@ -1,9 +1,13 @@
 var database = require('../database.js');
 
 module.exports = function(app) {
+
+    /**
+     * Homepage
+     */
     app.get('', function(req, res) {
         res.render('home', {
-
+            users: database.getUser("534f7a5f24ffcf95def997d8")
         });
     });
 }
