71108968293256be0ba7852b23cd02405af68055
Nicholas Clawson <nickclaw@gmail.com>
2014-04-16 20:58:15 -0700

    Added major modules
    Added
     * jade templates
     * compression
     * favicon
     * sass compression
     * JSON body parsing
     * static file serving

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 // get modules
-var express = require('express');
-
-// get routes
-var pageRouter = require('./routers/pageRouter.js');
+var express = require('express'),
+    compress = require('compression')(),
+    favicon = require('static-favicon'),
+    bodyParser = require('body-parser'),
+    sass = require('node-sass'),
+    jade = require('jade'),
+    path = require('path');
 
 // initialize app
 var app = express();
+app.set('port', process.env.PORT || 8080);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-pageRouter(app);
+// add middleware
+app.use('/static/styles', sass.middleware({
+    src: path.join(__dirname, 'public/styles/scss'),
+    dest: path.join(__dirname, 'public/styles'),
+    outputStyle: 'compressed'
+}));
+app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use('/static', compress);
+app.use(favicon(path.join(__dirname, 'public/favicon.ico')));
+app.use('/api', bodyParser());
+
+// add routes
+require('./routers/pageRouter.js')(app);
 
 // start server
-var server = app.listen(8080, function() {
+var server = app.listen(app.get('port'), function() {
     console.log('Server listening on ' + server.address().port);
 });
--- routers/pageRouter.js
+++ routers/pageRouter.js
@@ -1,5 +1,7 @@
 module.exports = function(app) {
     app.get('', function(req, res) {
-        res.send('hello world');
+        res.render('home', {
+
+        });
     });
 }
