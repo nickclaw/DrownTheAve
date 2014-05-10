06e69c64b0abfcf272ff3ad05e79266ea6275613
Nicholas Clawson <nickclaw@gmail.com>
2014-05-10 10:56:06 -0700

    Made admin api for bars

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -3,36 +3,84 @@ var db = require('../database.js'),
 
 module.exports = function(app, passport) {
 
-    app.get('/admin', util.admin, function(req, res) {
+    /******* BARS *******/
+    app.post('/admin/api/bar', util.admin, function(req, res) {
+        db.createBar(req.body)
+            .then(function(bar){
+                res.send(bar);
+            });
+    });
+
+    app.delete('/admin/api/bar/:id', util.admin, function(req, res) {
+        db.deleteBar(req.params.id)
+            .then(function(bar){
+                res.send(bar);
+            });
+    });
+
+    app.get('/admin/api/bar/:id', util.admin, function(req, res) {
+        db.getBar(req.params.id)
+            .then(function(bar){
+                res.send(bar);
+            });
+    });
+
+    app.put('/admin/api/bar/:id', util.admin, function(req, res) {
+        db.updateBar(req.params.id, req.body)
+            .then(function(bar){
+                res.send(bar);
+            });
+    });
+
+    app.post('/admin/api/bars', util.admin, stripFind, function(req, res) {
+        basicSearch(db.Bar, req.body)
+            .then(function(bars) {
+                res.send(bars);
+            });
+    });
+
+
+
+    /******* APPLICATION *******/
+    app.get('/admin*', util.admin, function(req, res) {
         res.render('admin', {
             user: req.user
         });
     });
+}
 
+/**
+ * Basic customizable search
+ * @param {Model} model
+ * @param {Object?} options
+ *                    options.find
+ *                    options.limit
+ *                    options.offset
+ *                    options.sort
+ *                    options.order
+ * @param {Function?} callback
+ * @return {Promise}
+ */
+function basicSearch(Model, options, callback) {
+    var sort = {};
+    sort[options.sort || '_id'] = options.order || 'asc';
 
-    // create REST functions for all models
-    ['Bar', 'Special', 'User'].forEach(function(model) {
+    return model
+        .find(options.find || {})
+        .limit(options.limit || 10)
+        .skip(options.offset || 0)
+        .sort(sort)
+        .exec(callback);
+}
 
-        /**
-         * Returns a collections of objects
-         */
-        app.get('/admin/' + model.toLowerCase() + 's', util.admin, function(req, res) {
-            var limit = req.query.limit || 20,
-                offset = req.query.offset || 0,
-                sort = {};
-
-            sort[req.query.sort || '_id'] = req.query.order || 'asc';
-
-            db[model]
-                .find({})
-                .limit(limit)
-                .skip(offset)
-                .sort(sort)
-                .exec()
-                .then(function(bars) {
-                    res.send(bars);
-                });
-        });
-
-    });
+/**
+ * Removes the find parameter from the body
+ * so that you can't abuse the database find methods
+ * from the browser.
+ */
+function stripFind(req, res, next) {
+    if (req.body.find) {
+        delete req.body.find;
+    }
+    next();
 }
