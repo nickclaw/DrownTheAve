e3cf71f992822a2d3ba974103fc3f2395ee6e93b
Nicholas Clawson <nickclaw@gmail.com>
2014-05-15 02:54:10 -0700

    Abstracted out cell sass, added browse search

--- public/scripts/src/controllers.js
+++ public/scripts/src/controllers.js
@@ -8,17 +8,33 @@ controllers
         function($scope, $http, $routeParams) {
             $scope.loading = true;
 
+            $scope.sortOptions = typeMap[$routeParams.type].sortOptions;
+            $scope.sort = "id";
+            $scope.order = "asc";
+            $scope.search = "";
+
             $scope.models = [];
             $scope.itemUrl = "/static/partial/item/" + $routeParams.type + "-item.html";
 
             $scope.loadMore = function() {
+                console.log($scope);
+
                 $scope.loading = true;
 
-                $http.post('/admin/api/' + $routeParams.type + 's', {sort:'name', offset: $scope.models.length})
+                $http.post('/admin/api/' + $routeParams.type + 's', {
+                        sort: $scope.sort,
+                        order: $scope.order,
+                        offset: $scope.models.length
+                    })
                     .success(function(data) {
                         $scope.loading = false;
                         $scope.models = $scope.models.concat(data);
                     });
+            }
+
+            $scope.load = function() {
+                $scope.models = [];
+                $scope.loadMore();
             }
 
             $scope.loadMore();
@@ -37,7 +53,13 @@ controllers
     ]);
 
 var typeMap = {
-    bar: {},
-    special: {},
-    user: {}
+    bar: {
+        sortOptions: ['name', 'website', 'id']
+    },
+    special: {
+        sortOptions: ['barName', 'id', 'deal']
+    },
+    user: {
+        sortOptions: ['profile.firstName', 'profile.lastName', 'id', 'local.username']
+    }
 }
